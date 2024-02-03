823ddffb177e37e5252147ce3839301b20c4ad67
mmorrow6109 <msmorrow6109@gmail.com>
2024-02-03 15:51:35 -0800

    init

---
+++ develop/models/index.js
@@ -0,0 +1,20 @@
+// import models
+const Product = require('./product');
+const Category = require('./category');
+const Tag = require('./tag');
+const ProductTag = require('./productTag');
+
+// Products belongsTo Category
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
