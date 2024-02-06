c4020c09e9af59edc1f0445a4fb5e2ea83627204
mmorrow6109 <msmorrow6109@gmail.com>
2024-02-06 13:40:00 -0800

    Associations defined

--- develop/models/index.js
+++ develop/models/index.js
@@ -4,13 +4,33 @@ const Category = require('./category');
 const Tag = require('./tag');
 const ProductTag = require('./product-tag');
 
-// Products belongsTo Category
+//Defining the relationships, or associations between the models
 
-// Categories have many Products
+// Products belongsTo (one to one) Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE', // if a category is deleted, so are the products in that category
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Categories have many (one to many) Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE', // if a category is deleted, so are the products in that category
+});
+
+// Products belongToMany (many to many) Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE', // if a product is deleted, it will delete the product_tag as well
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE', // if a tag is deleted, it will delete the product_tag as well
+});
 
 module.exports = {
   Product,
