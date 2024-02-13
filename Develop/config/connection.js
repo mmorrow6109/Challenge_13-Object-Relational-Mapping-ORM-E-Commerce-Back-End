864031fce173036c61a4a9144af80939da6230a7
mmorrow6109 <msmorrow6109@gmail.com>
2024-02-13 14:30:23 -0800

    changed connection to remove env

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,14 +2,12 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  database: 'ecommerce_db',
+  username: 'root',
+  password: 'mySQLTweezer6109!',
+  host: 'localhost'
+});
 
 module.exports = sequelize;
